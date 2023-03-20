5723d09cc12cfc0804f1f0b68a1da9c9029dc834
crisselene <criisalways@gmail.com>
2023-03-20 14:15:15 +0100

    muebles sin funcionar colisiones

--- src/FirstScene.js
+++ src/FirstScene.js
@@ -10,7 +10,8 @@ export default class FirstScene extends Phaser.Scene {
 		//this.load.image('mainroom', 'assets/Tilemaps/mainRoom.png');
 		//this.load.tilemapTiledJSON('mapa', '/assets/Tilemaps/mainRoom.json' );
 
-		this.load.image("tiles", "/assets/Tilemaps/Tiles.png");
+		this.load.image("tiles", "/assets/Tilesets/Tiles.png");
+		this.load.image("props", "/assets/Tilesets/Props.png");
 		this.load.tilemapTiledJSON('map','assets/Tilemaps/prueba1.json');
 
 		this.load.spritesheet("player", "/assets/player/player.png", {frameWidth: 16, frameHeight:24});
@@ -20,21 +21,29 @@ export default class FirstScene extends Phaser.Scene {
 		//mapeado
 		const map = this.make.tilemap({key: "map", tileWidth: 32, tileHeight:32 });
 		const tileset = map.addTilesetImage("paredes","tiles");
+		const tileset2 = map.addTilesetImage("muebles","props");
+		
 		const layer = map.createLayer("layer1", tileset, 0, 0);
 		const paredesLayer = map.createLayer("paredes", tileset, 0, 0);
+		const mueblesLayer = map.createLayer("muebles", tileset2, 0, 0);
+
 		//resize mapeado
 		layer.displayWidth = this.sys.canvas.width;
 		layer.displayHeight = this.sys.canvas.height;
 		paredesLayer.displayWidth = this.sys.canvas.width;
 		paredesLayer.displayHeight = this.sys.canvas.height;
+		mueblesLayer.displayWidth = this.sys.canvas.width;
+		mueblesLayer.displayHeight = this.sys.canvas.height;
 		
 		//player
-		this.player = new Player(this, 100, 100)
+		this.player = new Player(this, 370, 500)
 		
 		//collisions
+		this.physics.add.collider(this.player, mueblesLayer);
 		this.physics.add.collider(this.player, paredesLayer);
 		paredesLayer.setCollisionBetween(0,4);
 		paredesLayer.setCollisionBetween(50,54);
+		mueblesLayer.setCollisionBetween(136,140);
 	
 	
 	}
