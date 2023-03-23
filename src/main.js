fdbfb141b8e1aac8c201ba40192c7e8c57041f60
SaMuelete01 <105879110+dr4g0nWT@users.noreply.github.com>
2023-03-23 14:47:13 +0100

    cambio a ver si arreglo lo que revento ayer eros

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import Phaser from "phaser";
 import MainMenu from './MainMenu.js'
 import FirstScene from './Escenas/FirstScene.js'
 import DialogScene from './Escenas/DialogScene.js'
 import IntroScene from './Escenas/IntroScene.js'
+//import DialogModalPlugin from './Plugins/DialogModalPlugin.js'
 const config = {
 	type: Phaser.AUTO,
 	parent: 'app',
@@ -11,7 +11,21 @@ const config = {
 	physics: {
 		default: 'arcade',
 	},
-	scene: [MainMenu, FirstScene, DialogScene, IntroScene],
+	scene: {
+		preload: preload,
+		create: create,
+		MainMenu,
+		FirstScene,
+		DialogScene,
+		IntroScene
+	  }
 }
 
 export default new Phaser.Game(config)
+
+function preload () {
+	this.load.plugin('DialogModalPlugin', './dialog_plugin.js');
+	this.sys.install('DialogModalPlugin');
+	console.log(this.sys.dialogModal);
+}
+function create () {}
