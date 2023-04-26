a84266726d6707cc049c711738d9353cf5898051
Eros Guerrero <94256335+erosguerrero@users.noreply.github.com>
2023-04-26 11:04:25 +0200

    Outro

---
+++ src/Escenas/OutroScene.js
@@ -0,0 +1,36 @@
+
+export default class OutroScene extends Phaser.Scene{
+
+    constructor() {
+		super('OutroScene')
+        this.config = {
+            skipKeys: ['ENTER', 'SPACE', 'S'],
+        }
+        this.keyObjects = []
+	}
+
+    preload(){
+		this.load.video('outroVideo', 'assets/video/outtroLogo.mp4', 'loadeddata', false, false);
+    }
+
+    create(){
+        var vid = this.add.video(400, 300, 'outroVideo');
+        this.config.skipKeys.forEach((key, i) => {
+            this.keyObjects[i] = this.input.keyboard.addKey(key)
+            this.keyObjects[i].once('down', () => {
+                // Skip video
+                vid.setPaused(true);
+                this.scene.start('menuScene');
+            })
+        })
+        vid.on('complete', this.changescene, this);
+        vid.play(false);
+    }
+
+    changescene(){
+        this.scene.start('menuScene');
+    }
+
+
+
+}
